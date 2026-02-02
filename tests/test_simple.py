d936cdece525f2cb36ce68df98edf363bf025cf6
frank-murphy <murphy_fj@yahoo.com>
2026-02-02 11:57:41 -0800

    4 fix linting (#4)
    * add test for 1+1=2
    
    * overhaul addition engine
    
    * fix sys.exit() for lint
    
    ---------
    
    Co-authored-by: Frank Murphy <frankmurphy@fmailbox.com>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,8 +11,21 @@ def run_add_integers(*args):
         capture_output = True, text = True, check = False)
     return completed_process.stdout.strip(), completed_process.returncode
 
+# Test cases
 def test_add3():
     """Add three integers"""
     output, exitcode = run_add_integers("1", "2", "9")
     assert output == "12"
     assert exitcode == 0
+
+def test_add_one_and_one():
+    """Add 1 + 1 to get 2"""
+    output, exitcode = run_add_integers("1", "1")
+    assert output == "2"
+    assert exitcode == 0
+
+def test_neg_null_arg():
+    """Negative:  no input means no output and non-0 exit code"""
+    output, exitcode = run_add_integers()
+    assert output == ""
+    assert exitcode != 0
